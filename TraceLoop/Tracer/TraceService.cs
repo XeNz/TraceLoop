e960cec784678fb863a6a7779e0717f0b5447065
XeNz <alexander.willemsen@xentricator.be>
2020-12-09 18:24:09 +0100

    Initial commit

---
+++ TraceLoop/Tracer/TraceService.cs
@@ -0,0 +1,7 @@
+﻿namespace TraceLoop.Tracer
+{
+    public class TraceService
+    {
+        
+    }
+}
---
+++ TraceLoop/Tracer/TracertEntry.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.NetworkInformation;
+
+namespace TraceLoop.Tracer
+{
+    public class TracertEntry
+    {
+        public int HopID { get; set; }
+        public string Address { get; set; }
+        public string Hostname { get; set; }
+        public long ReplyTime { get; set; }
+        public IPStatus ReplyStatus { get; set; }
+    }
+}
